af010a38596827259b21efed107bfede41e3ef6f
lin <719206525@qq.com>
2019-12-27 11:04:18 +0800

    新增template和代码格式化

---
+++ template/demo/main.vue
@@ -0,0 +1,24 @@
+<template>
+  <Main>模版一</Main>
+</template>
+
+<script>
+import Main from 'public/components/Main';
+
+import methods from './src/methods';
+import computed from './src/computed';
+import watch from './src/watch';
+
+export default {
+  methods,
+  computed,
+  watch,
+  components: {
+    Main,
+  },
+};
+</script>
+
+<style lang="scss">
+@import 'public/sass/init.scss', './sass/style.scss';
+</style>
