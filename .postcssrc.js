fdc0167e4ddafaaddc435019fb48017bb29c2e45
lin <719206525@qq.com>
2020-03-25 15:12:30 +0800

    新增移动端适配

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,10 +1,26 @@
 // https://github.com/michael-ciniawsky/postcss-load-config
 
 module.exports = {
-  "plugins": {
-    "postcss-import": {},
-    "postcss-url": {},
+  plugins: {
+    'postcss-import': {},
+    'postcss-url': {},
     // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
-  }
+    autoprefixer: {},
+    'postcss-px-to-viewport': {
+      unitToConvert: 'rpx',
+      viewportWidth: 320,
+      unitPrecision: 5,
+      propList: ['*'],
+      viewportUnit: 'vw',
+      fontViewportUnit: 'vw',
+      selectorBlackList: [],
+      minPixelValue: 1,
+      mediaQuery: false,
+      replace: true,
+      exclude: [],
+      landscape: false,
+      landscapeUnit: 'vw',
+      landscapeWidth: 568,
+    },
+  },
 }
